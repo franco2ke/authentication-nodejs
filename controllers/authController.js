4d0ef7d7c009f9241daf58457526ed0d2e82a858
Francis Wafula <francis@paon.co.ke>
2023-06-20 06:49:45 +0300

    feature(signup): create signup handler (#10)
    - sanitize user signup input
    - validate user input
    - encrypt password
    - generate & send OTP
    - save details to database
    
    [finishes #SDT-30]

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,24 +1,31 @@
+const User = require('../models/userModel');
+// Helper function 1: Create and send activation code OTP
+// Called by a handler/controller function for both signup and login
+
 // A) Sign Up Handler
 exports.signup = async (req, res, next) => {
-  let newUser;
   try {
-    newUser = {
-      name: req.body.name,
+    const newUser = await User.create({
+      firstname: req.body.name,
       email: req.body.email,
       password: req.body.password,
       passwordConfirm: req.body.passwordConfirm,
-    };
+    });
+
+    // Generate otp & save to database
+    let otp = newUser.createOTP();
+    // TODO send otp via email
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        newUser,
+      },
+    });
   } catch (err) {
     // error caught by global handler so just forwarding is ok
     next(err);
   }
-
-  res.status(201).json({
-    status: 'success',
-    data: {
-      newUser,
-    },
-  });
 };
 
 // B) Default placeholder handler before implementation of custom functions
